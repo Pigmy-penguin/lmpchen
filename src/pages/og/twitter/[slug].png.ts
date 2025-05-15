6571f3f6ad63c7336d7f856217076227fafa12f7
Spratch <contact@josephclenet.fr>
2025-05-15 19:53:55 +0200

    feat(seo): open graph image twitter margin

--- src/pages/og/twitter/[slug].png.ts
+++ src/pages/og/twitter/[slug].png.ts
@@ -1,14 +1,14 @@
 import type { APIRoute } from "astro";
 import { type CollectionEntry } from "astro:content";
-import { getPostImageBuffer } from "../../og";
-import getAllPosts from "../../utils/getAllPosts";
+import { getPostImageBuffer } from "../../../og";
+import getAllPosts from "../../../utils/getAllPosts";
 
 export async function getStaticPaths() {
   const posts = await getAllPosts(true);
 
   return posts.map((post) => ({
     params: { slug: post.id },
-    props: { ...post, data: { ...post.data, twitter: false } }
+    props: { ...post, data: { ...post.data, twitter: true } }
   }));
 }
 
