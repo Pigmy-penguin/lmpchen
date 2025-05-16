5e10749ff11adc349fc39b928a32df069cd49116
Spratch <contact@josephclenet.fr>
2025-05-16 17:04:52 +0200

    fix(seo): open graph image color converter on twitter image

--- src/pages/og/twitter/[slug].png.ts
+++ src/pages/og/twitter/[slug].png.ts
@@ -2,13 +2,25 @@ import type { APIRoute } from "astro";
 import { type CollectionEntry } from "astro:content";
 import { getPostImageBuffer } from "../../../og";
 import getAllPosts from "../../../utils/getAllPosts";
+import oklchToHex from "../../../utils/colorConverter";
 
 export async function getStaticPaths() {
   const posts = await getAllPosts(true);
 
   return posts.map((post) => ({
     params: { slug: post.id },
-    props: { ...post, data: { ...post.data, twitter: true } }
+    props: {
+      ...post,
+      data: {
+        ...post.data,
+        twitter: true,
+        formattedColor: post.data.color
+          ? post.data.color.startsWith("oklch")
+            ? oklchToHex(post.data.color)
+            : post.data.color
+          : "#000000"
+      }
+    }
   }));
 }
 
